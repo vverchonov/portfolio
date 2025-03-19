6f8fb8f63ef09f9c4d43a287953a672e4f47a0a8
Viacheslav Verchonov <vverchonov@gmail.com>
2025-03-19 19:39:56 -0400

    size of the navbar for huge screens

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,104 +4,106 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="max-z bg-blue fixed w-full z-20 top-0 start-0">
-      <div className="flex flex-wrap items-center justify-between p-4">
-        <Link
-          className="flex items-center space-x-3 rtl:space-x-reverse scale-hover text-white text-2xl"
-          href="/"
-        >
-          VViacheslav
-        </Link>
-
-        <div className="flex md:order-2 space-x-3 items-center md:space-x-0 rtl:space-x-reverse">
-          <div className="flex gap-3 justify-center">
-            <Link
-              className="scale-hover text-white text-2xl"
-              href="https://www.linkedin.com/in/viacheslav-verchonov/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <i className="devicon-linkedin-plain"></i>
-            </Link>
-            <Link
-              className="scale-hover text-white text-2xl"
-              href="https://drive.google.com/file/d/10iQh9-wnpBgQxl3FbOhk0E6Tuw--4pAi/view?usp=sharing"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <i className="devicon-google-plain"></i>
-            </Link>
-            <Link
-              className="scale-hover text-white text-2xl"
-              href="https://github.com/vverchonov"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <i className="devicon-github-original"></i>
-            </Link>
-          </div>
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="flex flex-wrap items-center justify-between p-4">
+          <Link
+            className="flex items-center space-x-3 rtl:space-x-reverse scale-hover text-white text-2xl"
+            href="/"
           >
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
+            VViacheslav
+          </Link>
+
+          <div className="flex md:order-2 space-x-3 items-center md:space-x-0 rtl:space-x-reverse">
+            <div className="flex gap-3 justify-center">
+              <Link
+                className="scale-hover text-white text-2xl"
+                href="https://www.linkedin.com/in/viacheslav-verchonov/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="devicon-linkedin-plain"></i>
+              </Link>
+              <Link
+                className="scale-hover text-white text-2xl"
+                href="https://drive.google.com/file/d/10iQh9-wnpBgQxl3FbOhk0E6Tuw--4pAi/view?usp=sharing"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="devicon-google-plain"></i>
+              </Link>
+              <Link
+                className="scale-hover text-white text-2xl"
+                href="https://github.com/vverchonov"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="devicon-github-original"></i>
+              </Link>
+            </div>
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-        </div>
-        <div
-          className="items-center m-auto justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
-        >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-            <li>
-              <Link
-                href="/"
-                className="block text-white text-2xl scale-hover"
-                aria-current="page"
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
               >
-                About Me
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/contact"
-                className="block text-white text-2xl scale-hover"
-              >
-                Contact Me
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/projects"
-                className="block text-white text-2xl scale-hover"
-              >
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/404-page-you-can-type-any-url-and-get-it"
-                className="block text-white text-2xl scale-hover"
-              >
-                404 page
-              </Link>
-            </li>
-          </ul>
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="items-center m-auto justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul className="flex flex-col p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+              <li>
+                <Link
+                  href="/"
+                  className="block text-white text-2xl scale-hover"
+                  aria-current="page"
+                >
+                  About Me
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/contact"
+                  className="block text-white text-2xl scale-hover"
+                >
+                  Contact Me
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/projects"
+                  className="block text-white text-2xl scale-hover"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/404-page-you-can-type-any-url-and-get-it"
+                  className="block text-white text-2xl scale-hover"
+                >
+                  404 page
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
